bc959a3c5b51f74b536b20e0d6f9596cc5803eb8
Leaks Station <86381194+LKST1@users.noreply.github.com>
2021-08-31 02:06:34 +0200

    Update covidtracker.py
    New edited version for Social Media Buttons & New Search Button 
    
    - Soon New design and More Features SOON :D

--- covidtracker.py
+++ covidtracker.py
@@ -1,13 +1,23 @@
 # importing required modules for the tracker
-
+import tkinter 
 from tkinter import *
 import requests
 import webbrowser
+############################################## FIRST PART OF THE TK FOR view
+GUIWorkspace = Tk()
+GUIWorkspace.geometry('1100x550')
 
+GUIWorkspace.title("COVID-19 Case Tracker")
+font_tuple = ("Helvetica", 20)
+font_button_tuple = ("Helvetica", 12)
 # functions for displaying the data
-
-def World_Data():
-    # pass
+label_1 = Label(GUIWorkspace, font = font_tuple)
+label_1.pack(pady=10)
+label_countryData = Label(GUIWorkspace, font = font_tuple)
+label_countryData.pack(pady=10)
+############################################## FIRST PART OF THE TK FOR view
+def getWorld_Data():
+    pass
     covid_api = 'https://corona.lmao.ninja/v2/all?yesterday='
     covid_json = requests.get(covid_api).json()
     total = str(covid_json['cases'])
@@ -20,39 +30,46 @@ def World_Data():
     'Total number of recoveries = '+recovered+'\n'+"Currently active cases of COVID-19 = "+active+'\n'+"Cases per a million = " 
     +casesPer10to6+'\n'+"Total number of countries affected by the COVID-19 Pandemic = "+countriesAffected)
     label_1.configure(foreground="blue")
+getWorld_Data()
 
-def countryData(cName):
-    # pass
-    api = f'https://corona.lmao.ninja/v2/countries/{cName}?yesterday&strict&query'
+
+def countryData(countryN):
+    #pass
+    api = f'https://corona.lmao.ninja/v2/countries/{countryN}?yesterday&strict&query'
     json = requests.get(api).json()
-    country_Name = str(json['country'])
     casesConf = str(json['cases'])
+    country_fullName = str(json['country'])
     deathConf = str(json['deaths'])
     recoveredConf = str(json['recovered'])
+    activeConf = str(json['active'])
     criticalConf = str(json['critical'])
     activeConf = str(json['active'])
     casesPer10to6 = str(json['casesPerOneMillion'])
     testsConf = str(json['tests'])
-    data_label.config(text = f"Total Number of COVID-19 cases in {country_Name} = " +casesConf+'\n'+f"Total Number of deaths in {country_Name}= " +deathConf+'\n'+
-    f"Total number of recoveries in {country_Name} = "+recoveredConf+'\n'+f"Currently active cases of COVID-19 in {country_Name} = "+activeConf+'\n'+f"Cases per a million in {country_Name} = "
-    +casesPer10to6+'\n'+f"Critical cases in {country_Name} = "+criticalConf+'\n'+f"Number of COVID-19 tests done in {country_Name} = "+testsConf)
-
-def stateData(dName):
-    # pass
-    api = f'https://corona.lmao.ninja/v2/states/{dName}?yesterday='
-    json = requests.get(api).json()
-    state_Name = str(json['state'])
-    casesConf = str(json['cases'])
-    deathConf = str(json['deaths'])
-    activeConf = str(json['active'])
-    testsPer10to6 = str(json['testsPerOneMillion'])
-    testsConf = str(json['tests'])
-    state_label.config(text = f"Total Number of COVID-19 cases in {state_Name} = " +casesConf+'\n'+f"Total Number of deaths in {state_Name}= " +deathConf+'\n'+f"Currently active cases of COVID-19 in {state_Name} = "+activeConf+
-    '\n'+f"Total tests done in {state_Name} =" +testsConf+'\n'+f"Tests done per a million in {state_Name} = " +testsPer10to6)
+    label_countryData.config(text = f"Total Number of COVID-19 cases in {country_fullName} = " +casesConf+'\n'+f"Total Number of deaths in {country_fullName}= " +deathConf+'\n'+
+    f"Total number of recoveries in {country_fullName} = "+recoveredConf+'\n'+f"Currently active cases of COVID-19 in {country_fullName} = "+activeConf+'\n'+f"Cases per a million in {country_fullName} = " 
+    +casesPer10to6+'\n'+f"Critical cases in {country_fullName} = "+criticalConf+'\n'+f"Number of COVID-19 tests done in {country_fullName} = "+testsConf)
 
 
+def search_by_country(event):
+    #pass
+    countryName = country_searchbox.get().upper()
+    if countryName:
+        try :
+            countryData(countryName)
+            print(f"{countryName} Information Generated.")
+        except :
+            print(f"{countryName} is not defined.")
+            error_text.config(text=f"Error: {countryName} is not defined or not available.\nPlease double check your spelling.")
+            error_text.configure(foreground="red")
 
-# functions to open the web browser when clicking the button
+
+twitterimg_path = PhotoImage(file="images/twittericon.png")
+youtubeimg_path = PhotoImage(file="images/youtubeicon.png")
+searchimg_path = PhotoImage(file="images/searchimg.png")
+
+urlTwt = 'https://twitter.com/Neon_DEVFN'
+urlYt = 'https://www.youtube.com/channel/UCdqFdOJpMbyXGYnSAZdCX_Q'
 
 def openTwitter():
     webbrowser.open(urlTwt)
@@ -60,87 +77,22 @@ def openTwitter():
 def openYouTube():
     webbrowser.open(urlYt)
 
+twitterButton = Button(GUIWorkspace, font = font_button_tuple, image=twitterimg_path,cursor='hand2',border='0',command=openTwitter)
+twitterButton.place(x=1280,y=630)
+ytButton = Button(GUIWorkspace, font = font_button_tuple, image=youtubeimg_path,cursor='hand2',border='0',command=openYouTube)
+ytButton.place(x=1200,y=630)
 
-# writing  ui code
-
-urlTwt = 'https://twitter.com/Neon__DEV'
-urlYt = 'https://www.youtube.com/channel/UCdqFdOJpMbyXGYnSAZdCX_Q'
-
-
-# function to search individual country data
-
-def search_by_country(event):
-    # pass
-    countryName = searchbox.get()
-    stateName = searchbox.get()
-    if countryName:
-        try:
-            countryData(countryName)
-            error_text.config(text="")
-            print(f"Data Generated for {countryName}")
-        except:
-            print(f"{countryName} is not defined.")
-            error_text.config(text=f"Error: {countryName} is not defined or un-available.\nPlease double check your spelling.")
-            error_text.configure(foreground='red')
-    elif stateName:
-        try:
-            stateData(stateName)
-            error_text.config(text="")
-            print(f"Data Generated for {stateName}")
-        except:
-            print(f"{stateName} is not defined.")
-            error_text.config(text=f"Error: {stateName} is not defined or un-available.\nPlease double check your spelling.")
-
-# function to search individual state data
-
-# def search_by_state(event):
-    # pass
-#    stateName = searchbox.get()
-#    if stateName:
-#        try:
-#            stateData(stateName)
-#            error_text.config(text="")
-#            print(f"Data Generated for {stateName}")
-#        except:
-#            print(f"{stateName} is not defined.")
-#            error_text.config(text=f"Error: {stateName} is not defined or un-available.
-#            \nPlease double check your spelling.")
-#           error_text.configure(foreground='red')
-
-
-# writing tkinter ui code
-
-GUIWorkspace = Tk()
-GUIWorkspace.geometry('1100x550')
-GUIWorkspace.title("COVID-19 Case Tracker")
-font_tuple = ("calibri", 20)
-font_button_tuple = ("calibri", 12)
-
-
-twitterButton = Button(GUIWorkspace, font = font_button_tuple, text = "My Twitter!", height=1, command = openTwitter)
-twitterButton.pack(side='bottom')
-ytButton = Button(GUIWorkspace, font = font_button_tuple, text = "My YouTube!", height=1, command = openYouTube)
-ytButton.pack(side='bottom')
-
-label_1 = Label(GUIWorkspace, font = font_tuple)
-label_1.pack(pady=10)
-data_label = Label(GUIWorkspace, font = font_tuple)
-data_label.pack(pady=10)
-state_label = Label(GUIWorkspace, font = font_tuple)
-state_label.pack()
-searchbox = Entry(GUIWorkspace, font = font_tuple, text = "Search by Country:", width = 20)
-searchbox.pack()
-search_button = Button(GUIWorkspace, font = font_tuple, width = 15, text = "Search", command = search_by_country)
+############################################## SECOND PART OF THE TK FOR SEARCH
+country_searchbox = Entry(GUIWorkspace, font = font_tuple, text = "Search by Country:", width = 20)
+country_searchbox.pack()
+search_button = Button(GUIWorkspace, font = font_tuple, image= searchimg_path,cursor="hand2",border='0',command = search_by_country)
 search_button.pack()
 error_text = Label(GUIWorkspace, font = font_tuple)
 error_text.pack()
-
-
 GUIWorkspace.bind('<Return>', search_by_country)
-
-
-World_Data()
-
 GUIWorkspace.mainloop()
+############################################## SECOND PART OF THE TK FOR SEARCH
 
-# COVID19-Tracker made with love and Python by Neon (a.k.a --> Neonツ, Neon__DEV, Ne10-Neon).
+
+
+# COVID19-Tracker made with love and Python by Neon (a.k.a --> Neonツ, Neon__DEV, Ne10-Neon).s
